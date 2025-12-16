c81ccb265c69e123247eec944c672dd118a5c1bf
Ezhil sree J <144870412+EzhilsreeJ@users.noreply.github.com>
2025-12-17 01:33:32 +0530

    Update src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Ezhil Hotel &copy; {new Date().getFullYear()} | All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
